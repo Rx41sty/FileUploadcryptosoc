5b883a4cbbf37b7b08b5e1f5296c2c40e9048380
Giorgi Baramidze <gbaramidze1247@gmail.com>
2022-08-08 19:06:56 +0400

    Started adding tests

--- src/Test/auth.test.ts
+++ src/Test/auth.test.ts
@@ -1,4 +1,23 @@
 import request from 'supertest';
+import multer from 'multer';
+import {Request, Response, NextFunction } from 'express';
+import AWS from 'aws-sdk';
+
+
+
+jest.mock('aws-sdk');
+// let spy = jest.fn();
+// jest.mock('multer', () => {
+//   const multer = () => ({
+//     any: () => {
+//       return (req:Request, res:Response, next:NextFunction) => {
+//         return next(spy());
+//       };
+//     },
+//   });
+//   multer.memoryStorage = () => jest.fn();
+//   return multer;
+// });
 
 describe('Testing', function(){
   afterEach(() => {
@@ -6,5 +25,15 @@ describe('Testing', function(){
   });
 
   it('Test', async function() {
-  });
+    AWS.S3.prototype.upload = jest.fn().mockReturnValue({
+      promise: jest.fn().mockResolvedValue({})
+    });
+
+//     const buffer = Buffer.from('some data');
+
+// return request(server)
+//   .post('/route')
+//   .set('Authorization', 'bearer ' + token)
+//   .attach('name', buffer, 'custom_file_name.txt');
+//   });
 });
